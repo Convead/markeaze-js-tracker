ccc925959c30099c9840bfce8ad3376fd6b1fb51
Vladimir Savelyev <rostber@yandex.ru>
2019-05-28 17:28:12 +0300

    Assets version property for event. Bump version to 0.1.1

--- assets/javascripts/config.js
+++ assets/javascripts/config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  version: '0.1.0',
+  version: '0.1.1',
   trackerName: 'markeaze-js',
   debugMode: false,
   demoResponse: false,
   appKey: undefined,
   visitor: {},
   uid: undefined,
+  assetsVersion: null,
   cookieUid: '_mkz_dvc_uid',
   endpoint: 'tracker.markeaze.com',
-  airbrakeProject: 'markeaze', // TODO: Set value
-  airbrakeApiKey: '' // TODO: Set value
+  airbrakeProject: '229028',
+  airbrakeApiKey: '3927498a0d17867a76acf5aa97eba72d'
 }
--- assets/javascripts/webForms/wrapper.js
+++ assets/javascripts/webForms/wrapper.js
@@ -3,6 +3,7 @@ const liquid = new Liquid()
 const domEvent = require('../libs/domEvent')
 const helpers = require('../helpers')
 const eEmit = require('../libs/eEmit')
+const config = require('../config')
 
 export default class Wrapper {
   constructor (elContainer) {
@@ -10,17 +11,19 @@ export default class Wrapper {
     this.elContainer = elContainer || document.body
     this.assets = null
     this.el = null
+
+    const str = window.localStorage.getItem(this.assetsName)
+    if (str != null) {
+      this.assets = JSON.parse(str)
+      config.assetsVersion = this.assets.assets_version
+    }
   }
   assetsLoader (assets) {
-    if (!this.assets) {
-      const str = window.localStorage.getItem(this.assetsName)
-      if (!str) this.assets = JSON.parse(str)
-    }
-
     if (!assets) return false
 
     if (!this.assets || this.assets.version !== assets.version) {
       this.assets = assets
+      config.assetsVersion = this.assets.assets_version
       window.localStorage.setItem(this.assetsName, JSON.stringify(assets))
       return true
     }
@@ -29,6 +32,7 @@ export default class Wrapper {
   render (assets) {
     // Update for the first time or if there are changes
     if (this.assetsLoader(assets) || !this.el) {
+      if (!this.assets) return false
       this.el = document.querySelector('.mkz-js-main') || helpers.appendHTML(this.elContainer, this.assets.web_forms_common_wrapper)
       this.elRibbons = this.el.querySelector('.mkz-js-ribbons')
       this.elWebForms = this.el.querySelector('.mkz-js-wfs')

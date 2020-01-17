9e6f169a74bfceb14d9a24f35e4a2415c5faf675
Vladimir Savelyev <rostber@yandex.ru>
2020-01-17 14:04:53 +0300

    Change format of assets. Bump version to 1.1.2

--- assets/javascripts/assetsLoader.js
+++ assets/javascripts/assetsLoader.js
@@ -16,6 +16,7 @@ export default class AssetsLoader {
     if (!assets) return false
 
     if (!store.assets || store.assets.version !== assets.version) {
+      if (assets.chat_settings) assets.chat_settings = JSON.parse(assets.chat_settings)
       store.assets = Object.assign({}, store.assets, assets)
       window.localStorage.setItem(this.assetsName, JSON.stringify(assets))
       return true
--- assets/javascripts/store.js
+++ assets/javascripts/store.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: '1.1.1',
+  version: '1.1.2',
   plugins: {
     chat: {
       url: 'https://cdn.jsdelivr.net/gh/markeaze/markeaze-js-chat-client@latest/dist/mkz-chat-client.js',
